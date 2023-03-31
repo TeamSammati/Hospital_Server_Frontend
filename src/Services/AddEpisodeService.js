47ac5a3d60537fb16536ae4dc1b6e5e36215a99a
BOPPANA VENKATESH <boppanavenkatesh97@gmail.com>
2023-03-31 21:54:10 +0530

    Updated Services

--- src/Services/AddEpisodeService.js
+++ src/Services/AddEpisodeService.js
@@ -6,7 +6,7 @@ const addEpisode = async (requestParams) => {
     let token = window.localStorage.getItem('BearerToken')
     token=token.substring(1,token.length-1);
     console.log(requestParams)
-    const response = await axios.post(`${addEpisodeURL}?patientId=${requestParams.patientId}&episodetype=${requestParams.episodetype}`, {
+    const response = await axios.post(`${addEpisodeURL}?patientId=${requestParams.patientId}&episodetype=${requestParams.episodetype}`, null, {
         headers: {
         'Authorization': `Bearer ${token}`
         }
